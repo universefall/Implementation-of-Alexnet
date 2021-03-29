534da209c1651b75c903efe9ec59e3b1846c5a5e
Bill <zhangboyan_0823@163.com>
2021-03-29 16:01:15 +0800

    Add files via upload

---
+++ src/alexnet_inference.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""
+用于1000个分类的Alexnet网络测试，该版本去掉了LRN，后续的论文指出LRN对网络表现提升不大(奥卡姆剃刀)
+
+1.增加了AdaptiveAvgPool2d，对图片卷积图进行自适应池化，方便输入FC层
+2.改变了卷积核数量:conv1, conv2, conv3, conv4, conv5 
+                  ->64,  192,    384,   256,   256
+
+"""
+
+import os
+os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
+import time
+import json
+import torch.nn as nn
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+from matplotlib import pyplot as plt
+import torchvision.models as models
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def img_transform(img_rgb, transform=None):
+    """
+    加载图片 
+    将数据转换为模型读取的形式
+    :param img_rgb: PIL Image
+    :param transform: torchvision.transform
+    :return: tensor
+    """
+
+    if transform is None:
+        raise ValueError("找不到transform！必须有transform对img进行处理")
+
+    img_t = transform(img_rgb)
+    return img_t
+
+
+def load_class_names(p_clsnames, p_clsnames_cn):
+    """
+    加载标签名
+    :param p_clsnames:
+    :param p_clsnames_cn:
+    :return:
+    """
+    with open(p_clsnames, "r") as f:
+        class_names = json.load(f)
+    with open(p_clsnames_cn, encoding='UTF-8') as f:  # 设置文件对象
+        class_names_cn = f.readlines()
+    return class_names, class_names_cn
+
+
+def get_model(path_state_dict, vis_model=False):
+    """
+    创建模型，加载参数
+    :param path_state_dict:
+    :return:
+    """
+    model = models.alexnet()
+    pretrained_state_dict = torch.load(path_state_dict)
+    # 加载的模型为一个字典
+    model.load_state_dict(pretrained_state_dict)
+    model.eval()
+
+    if vis_model:
+        from torchsummary import summary
+        # 可视化模型的数据流
+        summary(model, input_size=(3, 224, 224), device="cpu")
+
+    model.to(device)
+    return model
+
+
+def process_img(path_img):
+
+    # hard code
+    norm_mean = [0.485, 0.456, 0.406]
+    norm_std = [0.229, 0.224, 0.225]
+    # transform对图片进行预处理，resize到256，然后中心裁剪到224
+    inference_transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(norm_mean, norm_std),
+    ])
+
+    # path --> img
+    img_rgb = Image.open(path_img).convert('RGB')
+
+    # img --> tensor
+    img_tensor = img_transform(img_rgb, inference_transform)
+    img_tensor.unsqueeze_(0)        # chw --> bchw 为tensor增加一个batch的维度
+    img_tensor = img_tensor.to(device)
+
+    return img_tensor, img_rgb
+
+
+if __name__ == "__main__":
+
+    # config
+    path_state_dict = os.path.join(BASE_DIR, "..", "data", "alexnet-owt-4df8aa71.pth")
+    # path_img = os.path.join(BASE_DIR, "..", "data", "Golden Retriever from baidu.jpg")
+    path_img = os.path.join(BASE_DIR, "..", "data", "tiger cat.jpg")
+    path_classnames = os.path.join(BASE_DIR, "..", "data", "imagenet1000.json")
+    path_classnames_cn = os.path.join(BASE_DIR, "..", "data", "imagenet_classnames.txt")
+
+    # load class names
+    cls_n, cls_n_cn = load_class_names(path_classnames, path_classnames_cn)
+
+    # 1/5 load img
+    img_tensor, img_rgb = process_img(path_img)
+
+    # 2/5 load model
+    alexnet_model = get_model(path_state_dict, True)
+
+    # 3/5 inference  tensor --> vector
+    # no_grad() 只做前向加快
+    with torch.no_grad():
+        time_tic = time.time()
+        outputs = alexnet_model(img_tensor)
+        time_toc = time.time()
+
+    # 4/5 index to class names
+    _, pred_int = torch.max(outputs.data, 1)
+
+    # topk找出前k大的value和对应的key值，此处为前5项error:
+    _, top5_idx = torch.topk(outputs.data, 5, dim=1) 
+
+    pred_idx = int(pred_int.cpu().numpy())
+    pred_str, pred_cn = cls_n[pred_idx], cls_n_cn[pred_idx]
+    print("img: {} is: {}\n{}".format(os.path.basename(path_img), pred_str, pred_cn))
+    print("time consuming:{:.2f}s".format(time_toc - time_tic))
+
+    # 5/5 visualization
+    plt.imshow(img_rgb)
+    plt.title("predict:{}".format(pred_str))
+    top5_num = top5_idx.cpu().numpy().squeeze()
+    text_str = [cls_n[t] for t in top5_num]
+    for idx in range(len(top5_num)):
+        plt.text(5, 15+idx*30, "top {}:{}".format(idx+1, text_str[idx]), bbox=dict(fc='yellow'))
+    plt.show()
+
